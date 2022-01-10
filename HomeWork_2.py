41a3ac7a991bb1aa41e9d3deef8f1f43125fad7e
AnastasiyaZv <88425646+AnastasiyaZv@users.noreply.github.com>
2022-01-10 19:21:19 +0300

    new

--- HomeWork_2.py
+++ HomeWork_2.py
@@ -1,15 +1,14 @@
 # Рисует узор, используя повтояющиеся круги
 import turtle
-NUM_CIRCLE = 36      # количество рисуемых кругов
-RADIUS = 100         # радиус каждого круга
-ANGLE = 10           # угол поворота
+
+NUM_CIRCLE = 36  # количество рисуемых кругов
+RADIUS = 100  # радиус каждого круга
+ANGLE = 10  # угол поворота
 ANIMATION_SPEED = 0  # скорость анимации
 
-turtle.speed (ANIMATION_SPEED) 
+turtle.speed(ANIMATION_SPEED)
 # нарисовать NUM_CIRCLE кругов, наклоняя черепаху на ANGLE градусов,
 # после того, как каждый круг был нарисован
-for x in range (NUM_CIRCLE):
+for x in range(NUM_CIRCLE):
     turtle.circle(RADIUS)
     turtle.left(ANGLE)
-
-    
